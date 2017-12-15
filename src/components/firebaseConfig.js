596881857908900376b58df12976bebb20379435
peterbata <peterbata@hotmail.com>
2017-12-15 16:34:35 -0500

    Initial Commit DEC1517

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default { 
+  apiKey: "AIzaSyCJKW2Ofk2P1lKyI6OXjby-RFpekE-xmn4",
+    authDomain: "vuefs-prod-a861c.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-a861c.firebaseio.com",
+    projectId: "vuefs-prod-a861c",
+    storageBucket: "vuefs-prod-a861c.appspot.com",
+    messagingSenderId: "594213039538"
+}
